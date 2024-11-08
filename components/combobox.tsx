db8220d3f94b22727f2447a9d5b73a3f63752b3e
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-11-08 10:59:54 -0500

    add the text inputs for the resume

---
+++ components/combobox.tsx
@@ -0,0 +1,82 @@
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon } from "lucide-react";
+import clsx from "clsx";
+import { useState } from "react";
+
+interface PeopleProps {
+  id: number;
+  name: string;
+}
+
+export default function Example({ people }: { people: PeopleProps[] }) {
+  const [query, setQuery] = useState<string>("");
+  const [selected, setSelected] = useState<PeopleProps[] | any>([]);
+  const [newOption, setNewOption] = useState<string>("");
+
+  const filteredPeople: PeopleProps[] =
+    query === ""
+      ? people
+      : people.filter((person) =>
+          person.name.toLowerCase().includes(query.toLowerCase())
+        );
+  console.log(newOption);
+  return (
+    <Combobox<PeopleProps[]>
+      value={selected}
+      onChange={(value) => setSelected(value)}
+    >
+      <div className="relative">
+        <ComboboxInput
+          multiple
+          className={clsx(
+            "w-full rounded-lg border-none bg-gray-600/5 py-1.5 pr-8 pl-3 text-sm/6 text-black",
+            "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black"
+          )}
+          displayValue={(person: PeopleProps | null) =>
+            person?.name ?? newOption
+          }
+          onChange={(event) => setQuery(event.target.value)}
+          onKeyDownCapture={() => setNewOption(query)}
+        />
+
+        <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+          <ChevronDownIcon className="size-4 fill-black group-data-[hover]:fill-white" />
+        </ComboboxButton>
+      </div>
+
+      <ComboboxOptions
+        className={clsx(
+          "w-[var(--input-width)] rounded-xl border border-white/5 bg-black p-1 [--anchor-gap:var(--spacing-1)] empty:invisible",
+          "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0"
+        )}
+      >
+        {query.length > 0 && (
+          <ComboboxOption
+            value={{ id: null, name: newOption }}
+            className="data-[focus]:bg-blue-100"
+          >
+            Create <span className="font-bold">"{newOption}"</span>
+          </ComboboxOption>
+        )}
+        {filteredPeople.map((person) => (
+          <ComboboxOption
+            key={person.id}
+            value={person}
+            className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+          >
+            <CheckIcon className="invisible size-4 fill-black ata-[focus]:bg-blue-100" />
+            <div className="text-sm/6 text-gray-400">
+              {person.name === "" ? newOption : person.name}
+            </div>
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
+  );
+}
--- components/generateResume.tsx
+++ components/generateResume.tsx
@@ -1,6 +1,15 @@
 import { ArrowLeft, ArrowRight } from "lucide-react";
+import Example from "./combobox";
 
 export function GenerateResume() {
+  const people = [
+    { id: 1, name: "MERN Stack" },
+    { id: 2, name: ".Net specific" },
+    { id: 3, name: "Deveops " },
+    { id: 4, name: "Javascript" },
+    { id: 5, name: "Azure" },
+  ];
+
   return (
     <div className="h-full  flex md:flex-row  flex-col ">
       <div className="w-full md:w-1/2  border flex flex-col justify-between ">
@@ -35,17 +44,55 @@ export function GenerateResume() {
           <h1 className="font-bold">Content</h1>
           <p>Paste the job description below</p>
           <textarea
-            placeholder="place your job description"
+            placeholder="Job Description Here please"
             className="w-full h-[20rem] bg-gray-200/40 p-2 rounded-lg"
           ></textarea>
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2 p-2">
             <h1 className="text-sm font-medium">
               Which skills should be focused?
             </h1>
+            <div className="p-2 relative">
+              <Example people={people} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 p-2">
+            <h1 className="text-sm font-medium">
+              How creative should the output be?
+            </h1>
+            <div className="p-2 relative">
+              <Example people={people} />
+            </div>
+          </div>
+          <div className="flex flex-row w-full gap-2 p-2">
+            <div className="flex-col gap-2 w-full flex">
+              <h1 className="text-sm font-medium">First Name</h1>
+              <input
+                placeholder="firstname"
+                className="w-full bg-gray-300 rounded-lg p-2"
+              ></input>
+            </div>
+            <div className="flex-col gap-2 w-full flex">
+              <h1 className="text-sm font-medium">Last Name</h1>
+              <input
+                placeholder="firstname"
+                className="w-full bg-gray-300 rounded-lg p-2"
+              ></input>
+            </div>
+          </div>
+          <div className="w-full flex  justify-center items-center">
+            <button className="bg-blue-300 p-2 rounded-lg">
+              Create Content
+            </button>
           </div>
         </div>
       </div>
-      <div className="w-1/2">Second half</div>
+      <div className="w-1/2 h-full">
+        <div className="flex text-md border h-1/2 justify-between w-full p-3  items-end ">
+          <span className="cursor-pointer hover:border-b p-2  font-bold ">
+            Preview
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
